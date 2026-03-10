825e5518bb24fd336215c836f4a600566d52a5e2
fei01010 <2025311510@stu.hit.edu.cn>
2026-03-10 21:51:44 +0800

    修改1

--- LG/B3910.c
+++ LG/B3910.c
@@ -1,24 +1,14 @@
 #include<stdio.h>
-#include<stdlib.h>
 
 int main(){
-    int m;
-    scanf("%d",&m);
-    if(m < 1 || m > 100000){
-        printf("error!");
-        return 0;
-    }
-    int n = m + 1;
-    int a[n];
-    for(int i = 0;i < n;i ++){
-        scanf("%d",&a[i]);
-        if(a[i] == 0 || abs(a[i]) > 1000000000){
-            printf("error!");
-            return 0;
-        }
+    int n;
+    scanf("%d",&n);
+    long long a[100001];
+    for(int i = 0;i <= n;i ++){
+        scanf("%lld",&a[i]);
     }
     int cnt = 0;
-    for(int i = 0;i < n - 1;i ++){
+    for(int i = 0;i <= n - 1;i ++){
         if(a[i] * a[i+1] < 0){
             cnt ++;
         }
