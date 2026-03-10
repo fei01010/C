ab3a6e818a6c38258e2e7022cb6a2e105c4c4686
fei01010 <2025311510@stu.hit.edu.cn>
2026-03-10 22:36:58 +0800

    方案一

--- LG/P1002.c
+++ LG/P1002.c
@@ -1,5 +1,47 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
+int main() {
+    int n, m, hx, hy;
+    scanf("%d %d %d %d", &n, &m, &hx, &hy);
+
+    int blocked[25][25] = {0};
+    long long dp[25][25] = {0};
+
+    int dx[8] = {1, 2, 2, 1, -1, -2, -2, -1};
+    int dy[8] = {2, 1, -1, -2, -2, -1, 1, 2};
+
+    if (hx >= 0 && hx <= n && hy >= 0 && hy <= m) {
+        blocked[hx][hy] = 1;
+    }
+    for (int i = 0; i < 8; i++) {
+        int x = hx + dx[i];
+        int y = hy + dy[i];
+        if (x >= 0 && x <= n && y >= 0 && y <= m) {
+            blocked[x][y] = 1;
+        }
+    }
+
+    if (!blocked[0][0]) {
+        dp[0][0] = 1;
+    }
+
+    for (int i = 0; i <= n; i++) {
+        for (int j = 0; j <= m; j++) {
+            if (blocked[i][j]) {
+                continue;
+            }
+            if (i == 0 && j == 0) {
+                continue;
+            }
+            if (i > 0) {
+                dp[i][j] += dp[i - 1][j];
+            }
+            if (j > 0) {
+                dp[i][j] += dp[i][j - 1];
+            }
+        }
+    }
+
+    printf("%lld", dp[n][m]);
     return 0;
 }
