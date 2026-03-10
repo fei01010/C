a9beeb4b943537b85f99442911a87afbd0a3c2e6
fei01010 <2025311510@stu.hit.edu.cn>
2026-03-10 22:59:19 +0800

    P1009ai版

--- LG/P1009.c
+++ LG/P1009.c
@@ -1,18 +1,48 @@
-#include<stdio.h>
-long long factorial(int x){
-    if(x==0||x==1)return 1;
-    else return x*factorial(x-1);
-}
-int main(){
+#include <stdio.h>
+
+int main() {
     int n;
-    scanf("%d",&n);
-    if(n>50||n<1){
-        scanf("%d",&n);
+    int factorial[200] = {0};
+    int sum[200] = {0};
+    int factorial_len = 1;
+    int sum_len = 1;
+
+    scanf("%d", &n);
+
+    factorial[0] = 1;
+    sum[0] = 0;
+
+    for (int i = 1; i <= n; i++) {
+        int carry = 0;
+        for (int j = 0; j < factorial_len; j++) {
+            int value = factorial[j] * i + carry;
+            factorial[j] = value % 10;
+            carry = value / 10;
+        }
+        while (carry > 0) {
+            factorial[factorial_len] = carry % 10;
+            carry /= 10;
+            factorial_len++;
+        }
+
+        carry = 0;
+        int max_len = factorial_len > sum_len ? factorial_len : sum_len;
+        for (int j = 0; j < max_len; j++) {
+            int value = sum[j] + factorial[j] + carry;
+            sum[j] = value % 10;
+            carry = value / 10;
+        }
+        sum_len = max_len;
+        while (carry > 0) {
+            sum[sum_len] = carry % 10;
+            carry /= 10;
+            sum_len++;
+        }
     }
-    long long sum = 0;
-    for(int i = 1;i <= n; i++){
-        sum += factorial(i);
+
+    for (int i = sum_len - 1; i >= 0; i--) {
+        printf("%d", sum[i]);
     }
-    printf("%lld",sum);
+
     return 0;
 }
